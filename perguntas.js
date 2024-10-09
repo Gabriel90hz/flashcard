b18cc077460df2ef3455da639b8838ff953baa85
Gabriel90hz <159458555+Gabriel90hz@users.noreply.github.com>
2024-10-09 11:29:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'Futebol',
+    'Quyal o jogador com mais bola de ouro?',
+    'O Messi'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'biologia',
+    'Qual o maior animal terrestre?',
+    'O elefante africano'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é BYE'
+)
+
+criaCartao(
+    'sociologia',
+    'O que é sociologia?',
+    'É o estudo da sociedade'
+)
+
+criaCartao(
+    'Portugues',
+    'O que é verbo?',
+    'Indica uma ação'
+)
+
+criaCartao(
+    'Robotica',
+    'O que é arduino?',
+    'E uma placa que utilizamos para montar sistemas'
+)
+
+criaCartao(
+    'Futebol',
+    'O maior artilheiro do futebol?',
+    'Cristiano Ronaldo'
+)criaCartao(
+    'Programação',
+    'O que é codigo?',
+    'O que faz o sistema funcionar'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Italia?',
+    'A capital da italia é roma'
+)
